d93b7d2500e63fb8a2f76fbd9432578409f931c1
Anton Shapovalov <anton.shapovalov@chisw.com>
2018-06-29 18:27:38 +0300

    addIngredientHandler added

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,7 +5,9 @@ const buildControl = props => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label}</div>
     <button className={classes.Less}>Less</button>
-    <button className={classes.Less}>More</button>
+    <button onClick={props.added} className={classes.Less}>
+      More
+    </button>
   </div>
 );
 
