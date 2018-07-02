4d42be3142fd72b25621fc125f32cc3765ca131e
Anton Shapovalov <anton.shapovalov@chisw.com>
2018-07-03 00:47:06 +0300

    optimazed rerender in Modal component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,7 +6,7 @@ import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 class Layout extends Component {
   state = {
-    showSideDraw: true
+    showSideDraw: false
   };
   sideDrawerClosedHandler = () => {
     this.setState({ showSideDraw: false });
